c237f5d442a1df65b7e883e2b44de28e8da780a9
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2024-09-18 02:38:25 +0100

    feat: show token

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [accessToken, setAccessToken] = useState(null);
 
   useEffect(() => {
     Office.onReady().then(() => {
@@ -21,7 +19,7 @@ function App() {
         if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
           var accessToken = asyncResult.value;
           // Use the access token to make authenticated requests
-          console.log(accessToken);
+          setAccessToken(accessToken);
         } else {
           console.error(asyncResult.error.message);
         }
@@ -37,25 +35,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        access token: {accessToken}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
