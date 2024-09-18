e09fa2d821d4fbc7689754f8221ad7643db00b06
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2024-09-18 23:43:04 +0100

    feat: run locally on ssl

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // @ts-expect-error
+  const env = loadEnv(mode, process.cwd());
+  if (env.VITE_ENVIRONMENT === 'development') {
+    return {
+      plugins: [
+        react(),
+        basicSsl()
+      ]
+    }
+  }
+
+
+  return {
+    plugins: [
+      react(),
+    ]
+  }
 })
